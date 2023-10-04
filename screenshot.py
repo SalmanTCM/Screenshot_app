8fce4bdaba47ebd2745b707756d8a9fae3244527
SalmanTechnometrics <salman@technometrics.net>
2023-10-04 11:49:03 +0600

    button added

--- screenshot.py
+++ screenshot.py
@@ -5,11 +5,21 @@ import tkinter as tk
 def screenshot():
     name = int(round(time.time() * 1000))
     name = './Screenshots/{}.png'.format(name)
-    time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
 
-screenshot()
-# root = tk.Tk()
-# frame = tk.Frame (root)
-# frame.pack
+root = tk.Tk()
+frame = tk.Frame (root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text= "Take Screenshot",
+    command = screenshot)
+button.pack(side=tk.LEFT)
+close = tk.Button(
+    frame,
+    text = "Quit",
+    command= quit)
+close.pack(side=tk.LEFT)
+root.mainloop()
